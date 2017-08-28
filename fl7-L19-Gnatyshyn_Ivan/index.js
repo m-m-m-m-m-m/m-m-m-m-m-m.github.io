f324b85899934dab1ad935c75b633ff923252508
m-m-m-m-m-m <535469@ukr.net>
2017-08-28 23:41:25 +0300

    1

--- fl7-L19-Gnatyshyn_Ivan/index.js
+++ fl7-L19-Gnatyshyn_Ivan/index.js
@@ -5,7 +5,7 @@
 
 
 function makeFetch(){
-    return fetch('http://marsweather.ingenology.com/v1/latest', {mode: 'cors'})
+    return fetch('https://cors-anywhere.herokuapp.com/marsweather.ingenology.com/v1/latest/', {mode: 'cors'})
         .then(function(response) {
             return response.text();
         })
